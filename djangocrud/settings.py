45b9cf5f6032edfdd94a1c4dfc863d8dc4f6b7d2
JuanNahual <juan.nahueljg@gmail.com>
2022-11-15 14:05:12 -0300

    modificando setting

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -82,8 +82,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres')
+    'default': dj_database_url.config()
 }
 
 
