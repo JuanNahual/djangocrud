da1f3eeb0ff8f45c77701069ba95f44282361cfd
JuanNahual <juan.nahueljg@gmail.com>
2022-11-15 13:46:55 -0300

    modificando build and setting

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -83,9 +83,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_agre=600
-        )
+        default='postgresql://postgres:postgres@localhost/postgres')
 }
 
 
